8794b31feecb052722eccd0d63b79c94dcaa4057
maurizio <maurizio.sambati@gmail.com>
2016-05-16 22:43:10 +0200

    add simple monte carlo tree search engine

--- c4/engine/__init__.py
+++ c4/engine/__init__.py
@@ -1,6 +1,7 @@
 from c4.engine.base import Engine
 from c4.engine.greedy import GreedyEngine, WeightedGreedyEngine
 from c4.engine.random import RandomEngine
+from c4.engine.mcts import MonteCarloTreeSearch
 from c4.engine.negamax import NegamaxEngine
 from c4.engine.alphabeta import AlphaBetaEngine, ABCachedEngine, ABDeepEngine
 from c4.engine.pvs import PVSEngine, PVSCachedEngine, PVSDeepEngine
@@ -10,6 +11,7 @@ __all__ = ['Engine',
            'GreedyEngine',
            'WeightedGreedyEngine',
            'RandomEngine',
+           'MonteCarloTreeSearch',
            'NegamaxEngine',
            'AlphaBetaEngine',
            'ABCachedEngine',
---
+++ c4/engine/mcts.py
@@ -0,0 +1,111 @@
+import math
+from collections import defaultdict
+
+from c4.engine.base import Engine
+from c4.engine.greedy import WeightedGreedyEngine
+#from c4.engine.random import RandomEngine
+from c4.evaluate import DRAW
+
+
+class MonteCarloTreeSearch(Engine):
+    def __init__(self, simulations=1000, C=1/math.sqrt(2)):
+        super(MonteCarloTreeSearch, self).__init__()
+        self.simulations = int(simulations)
+        self.C = float(C)
+        self.simulation_engine = WeightedGreedyEngine(False)
+
+    def choose(self, board):
+        stats = self.search(board, self.simulations, self.C)
+        return self.select_best_move(stats, board)
+
+    def search(self, board, simulations, C):
+        stats = defaultdict(lambda: [0, 0])
+        k = board.hashkey()
+        actions = list(board.moves())
+        for m in actions:
+            stats[k, m] = [0, 0]
+
+        root = board
+        for i in range(simulations):
+            node = root
+            states = [node.hashkey()]
+            transactions = []
+
+            # select leaf node
+            while node.end is None:
+                move, expand = self.select_next_move(stats, node, C)
+                transactions.append(move)
+                node = node.move(move)
+                states.append(node.hashkey())
+
+                if not expand:
+                    break
+
+            # run simulation if not at the end of the game tree
+            if node.end is None:
+                result = self.simulate(node)
+            else:
+                result = node.end
+                if result != 0:
+                    result = -1.0
+
+            # propagate results
+            for state, move in list(zip(states[:-1], transactions))[::-1]:
+                result = -result
+                stats[state, move][0] += 1
+                stats[state, move][1] += result
+
+        return stats
+
+    def simulate(self, board):
+        engine = self.simulation_engine
+        node = board
+        while node.end is None:
+            m = engine.choose(node)
+            node = node.move(m)
+
+        if node.end == DRAW:
+            return 0
+        elif node.end == board.stm:
+            return 1
+        else:
+            return -1
+
+    def select_next_move(self, stats, board, C):
+        """Select the next state and consider if it should be expanded"""
+
+        k = board.hashkey()
+        bestscore = None
+        bestmove = None
+        total_n = sum(stats[k, m][0] for m in board.moves())
+
+        for m in board.moves():
+            n, w = stats[k, m]
+            if n == 0:
+                return m, False
+            else:
+                score = (w / n) + C * math.sqrt(2 * math.log(total_n) / n)
+                if bestscore is None or score > bestscore:
+                    bestscore = score
+                    bestmove = m
+
+        assert bestmove is not None
+        return bestmove, True
+
+    def select_best_move(self, stats, board):
+        """Select the best move at the end of the Monte Carlo tree search"""
+
+        bestscore = 0
+        bestmove = None
+        k = board.hashkey()
+        for m in board.moves():
+            n, w = stats[k, m]
+            print('Move %d score: %d/%d' % (m+1, w, n))
+            if n > bestscore:
+                bestmove = m
+                bestscore = n
+        assert bestmove is not None
+        return bestmove
+
+    def __str__(self):
+        return 'MCTS(%s, %0.2f)' % (self.simulations, self.C)
