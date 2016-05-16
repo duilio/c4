a8e713750d741155c1ceb6a3e558d8b8f3ab72e9
maurizio <maurizio.sambati@gmail.com>
2016-05-16 23:16:11 +0200

    detect blocked threats in moveordering

--- c4/moveorder.py
+++ c4/moveorder.py
@@ -57,9 +57,13 @@ class MoveOrder(object):
 
 def evaldiff(board, m):
     r = board.freerow(m)
+
     stm = board.stm
     other = board.other
+
+    threat = False
     score = 0
+
     for s in Board.segments_around(board, r, m):
         c = np.bincount(s, minlength=3)
         c[0] -= 1
@@ -73,5 +77,15 @@ def evaldiff(board, m):
         elif c[stm] == 1:
             # remove enemy advantages on this area
             score += c[other] ** 2
+
+            # check if this is a threat, without this move the opponent has a
+            # chance to win
+            if c[0] == 0:
+                threat = True
+
+    # let's give a very high score to blocked threats as it is almost like a
+    # victory to block an opponent mate
+    if threat:
+        return INF - 1
 
     return score
