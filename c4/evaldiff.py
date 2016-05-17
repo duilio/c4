c943d4503f613cf231fdef658c8147d471dfc97d
maurizio <maurizio.sambati@gmail.com>
2016-05-17 15:12:56 +0200

    move evaldiff to a separate file

---
+++ c4/evaldiff.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from c4.board import Board
+from c4.evaluate import INF
+
+def evaldiff(board, m):
+    r = board.freerow(m)
+
+    stm = board.stm
+    other = board.other
+
+    threat = 0
+    score = 0
+
+    for s in Board.segments_around(board, r, m):
+        c = np.bincount(s, minlength=3)
+        c[0] -= 1
+        c[stm] += 1
+
+        if c[0] + c[stm] == 4:
+            # add player advantages on this area
+            if c[stm] == 4:
+                return INF
+            score += (c[stm] - 1) ** 2
+        elif c[stm] == 1:
+            # remove enemy advantages on this area
+            score += c[other] ** 2
+
+            # check if this is a threat, without this move the opponent has a
+            # chance to win
+            if c[0] == 0:
+                threat = 1
+
+    # let's give a very high score to blocked threats as it is almost like a
+    # victory to block an opponent mate
+    if threat:
+        return INF - 1
+
+    return score
--- c4/moveorder.py
+++ c4/moveorder.py
@@ -1,10 +1,8 @@
 import random
 from functools import partial
 
-import numpy as np
-
-from c4.board import Board
-from c4.evaluate import Evaluator, INF
+from c4.evaluate import Evaluator
+from c4.evaldiff import evaldiff
 
 
 class MoveOrder(object):
@@ -53,39 +51,3 @@ class MoveOrder(object):
             if x == hint:
                 continue
             yield x
-
-
-def evaldiff(board, m):
-    r = board.freerow(m)
-
-    stm = board.stm
-    other = board.other
-
-    threat = False
-    score = 0
-
-    for s in Board.segments_around(board, r, m):
-        c = np.bincount(s, minlength=3)
-        c[0] -= 1
-        c[stm] += 1
-
-        if c[0] + c[stm] == 4:
-            # add player advantages on this area
-            if c[stm] == 4:
-                return INF
-            score += (c[stm] - 1) ** 2
-        elif c[stm] == 1:
-            # remove enemy advantages on this area
-            score += c[other] ** 2
-
-            # check if this is a threat, without this move the opponent has a
-            # chance to win
-            if c[0] == 0:
-                threat = True
-
-    # let's give a very high score to blocked threats as it is almost like a
-    # victory to block an opponent mate
-    if threat:
-        return INF - 1
-
-    return score
