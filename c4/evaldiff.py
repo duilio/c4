a7960193acb93f91a59af1fb33f9990bf686d381
maurizio <maurizio.sambati@gmail.com>
2016-05-18 01:03:20 +0200

    further optimizations in evaldiff using lookup tables

--- c4/evaldiff.py
+++ c4/evaldiff.py
@@ -3,38 +3,20 @@ import numpy as np
 from c4.board import Board
 from c4.evaluate import INF
 
+from c4.tables import evaldiff_lookup, evaldiff_threat_lookup
 
-def evaldiff(board, m):
+
+def evaldiff(board, m, weights=np.array([1, 3, 9, 27], dtype=int)):
     r = board.freerow(m)
-
     stm = board.stm
-    other = board.other
+    indices = np.dot(Board.segments_around(board, r, m),
+                     weights)
+    partial_scores = evaldiff_lookup[stm][indices]
 
-    threat = 0
-    score = 0
+    if (partial_scores == 4**2).any():
+        return INF
 
-    for s in Board.segments_around(board, r, m):
-        c = np.bincount(s, minlength=3)
-        c[0] -= 1
-        c[stm] += 1
-
-        if c[0] + c[stm] == 4:
-            # add player advantages on this area
-            if c[stm] == 4:
-                return INF
-            score += (c[stm] - 1) ** 2
-        elif c[stm] == 1:
-            # remove enemy advantages on this area
-            score += c[other] ** 2
-
-            # check if this is a threat, without this move the opponent has a
-            # chance to win
-            if c[0] == 0:
-                threat = 1
-
-    # let's give a very high score to blocked threats as it is almost like a
-    # victory to block an opponent mate
-    if threat:
+    if evaldiff_threat_lookup[stm][indices].any():
         return INF - 1
 
-    return score
+    return partial_scores.sum()
--- c4/tables.py
+++ c4/tables.py
@@ -1,5 +1,16 @@
 import numpy as np
+import itertools
 
+#
+# Segment tables
+#
+# Segments are quartets of indices that represent four squares aligned and
+# consecutive in the board.
+# If a segment contains piece of the same player, that player won the game.
+#
+# all_segments is a 2d array, each row is a segment
+# rev_segments is an index square -> group of segments that pass by the square
+#
 
 all_segments = []
 rev_segments = [[] for x in range(7*6)]
@@ -25,5 +36,49 @@ for idx in (_indices, _indices[:, ::-1]):
         diag = idx.diagonal(di)
         add_rev(diag)
 
+
 all_segments = np.asarray(all_segments)
 rev_segments = np.asarray([np.asarray(x) for x in rev_segments])
+
+
+#
+# evaldiff lookup tables
+#
+
+# keep the scores of segment combos
+evaldiff_lookup = {
+    1: np.zeros(3**4, dtype=int),
+    2: np.zeros(3**4, dtype=int),
+}
+# used to check if the opponent would win if we don't fill the empty square
+evaldiff_threat_lookup = {
+    1: np.zeros(3**4, dtype=int),
+    2: np.zeros(3**4, dtype=int),
+}
+
+for comb in itertools.product(range(3), range(3), range(3), range(3)):
+    c = [0, 0, 0]
+    score1 = 0
+    score2 = 0
+
+    for x in comb:
+        c[x] += 1
+
+    if c[0] == 4:
+        score1 = 1
+        score2 = 1
+    elif c[0] + c[1] == 4:
+        score1 = (c[1] + 1) ** 2
+        score2 = c[1] ** 2
+    elif c[0] + c[2] == 4:
+        score2 = (c[2] + 1) ** 2
+        score1 = c[2] ** 2
+
+    key = np.dot(np.array(comb, dtype=int),
+                 np.array([3**0, 3**1, 3**2, 3**3], dtype=int))
+    evaldiff_lookup[1][key] = score1
+    evaldiff_lookup[2][key] = score2
+    if score2 == 4 ** 2:
+        evaldiff_threat_lookup[1][key] = 1
+    elif score1 == 4 ** 2:
+        evaldiff_threat_lookup[2][key] = 1
